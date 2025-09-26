6cc25dabcba6cf2bc943ca422e3ea2f1a3c3c6d2
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2025-09-26 21:55:41 +0100

    feat: mix phx.gen.auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :expire, :scopes,
+  user: [
+    default: true,
+    module: Expire.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Expire.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :expire,
   ecto_repos: [Expire.Repo],
   generators: [timestamp_type: :utc_datetime]
